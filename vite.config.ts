c1d1581cd189ab999c8770381bc0d91c88d82f8e
pascalbianchi-ops <pascal.bianchi@gmail.com>
2025-08-23 19:54:13 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        host: true,       // tu gardes
-        port: 5173,       // tu gardes
-        proxy: {
-            '/api': {
-                target: 'http://localhost:3000', // ton serveur de mots
-                changeOrigin: true
-                // pas besoin de rewrite: ton backend sert déjà /api/words
-            }
-        }
+  plugins: [react()],
+  base: '/worduo/',   // <-- trÃ¨s important pour GitHub Pages (nom exact du repo)
+  server: {
+    host: true,
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true
+        // pas besoin de rewrite: ton backend sert dÃ©jÃ  /api/words
+      }
     }
+  }
 })
-
-
-
