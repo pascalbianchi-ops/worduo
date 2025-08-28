e701d5f66ce25224422c5cae1aa9f8c8ddafa3fa
rpb268m <pascal.bianchi@gmail.com>
2025-08-28 16:55:57 +0200

    fix(vite): base='/' for Render

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/worduo/',   // <-- très important pour GitHub Pages (nom exact du repo)
-  server: {
-    host: true,
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true
-        // pas besoin de rewrite: ton backend sert déjà /api/words
-      }
+    plugins: [react()],
+    base: '/', // <-- IMPORTANT pour Render (app à la racine)
+    server: {
+        host: true,
+        port: 5173,
+        proxy: {
+            '/api': { target: 'http://localhost:3000', changeOrigin: true },
+            '/socket.io': { target: 'http://localhost:3000', ws: true }
+        }
     }
-  }
 })
+
+
